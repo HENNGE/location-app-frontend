3657c3aca91689331b9dd8ba6986a8a378abafbd
Edward Peng <edwardnz2017@gmail.com>
2024-09-18 10:52:00 +0900

    feat: add testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import tailwind from 'tailwindcss';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    test: {
+        coverage: {
+            provider: 'istanbul',
+        },
+        watch: true,
+    },
     plugins: [react()],
     css: {
         postcss: {
